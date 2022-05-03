5b2759cd4a36792c1a56ab5f0db7375a8c136fdd
longy <longy@sucsoft.com>
2022-05-04 00:17:53 +0800

    feat:引入模板

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index'
 import { createPinia } from 'pinia'
-
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
